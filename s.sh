1628c6f21f4829f5b9add0acfc2d877e81243865
slawomirpiela <d16124412@manager.c.d16124412.internal>
2017-12-04 00:41:52 +0000

    final final

--- s.sh
+++ s.sh
@@ -47,7 +47,7 @@ do
 		curl -s -X DELETE -H 'Accept: application/json' http://localhost:8080/deleteContainer/$container
 	    ;;
         "Delete all containers (including running)"
-		curl -s -X DELETE -H 'Accept: application/json'/ http://localhost:8080/deleteContainers
+		curl -s -X DELETE -H 'Accept: application/json' http://localhost:8080/deleteContainers
             ;;
         "Delete a specific image")
 	echo -e "Please enter an image to delete: "
